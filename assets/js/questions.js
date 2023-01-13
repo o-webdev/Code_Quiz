a1c56e579d888db4914c10f33e3cfe0bcc83dbdc
Omar-ahm <irycevier@gmail.com>
2023-01-13 19:17:48 +0000

    quiz questions added to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,34 @@
+var questions = [
+    {
+        question: "How can you add an element at the beginning of an array in JavaScript?",
+        choices: ["array.unshift(element)", "array.push(element)", "array.shift(element)"],
+        answer: "array.unshift(element)"
+    },
+    {
+        question: "How can you remove the last element from an array in JavaScript?",
+        choices: ["<script>", "<js>", "<javascript>", "<scripting>"],
+        answer: "<script>"
+    },
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        choices: ["alert('Hello World')", "msg('Hello World')", "msgBox('Hello World')", "alertBox('Hello World')"],
+        answer: "alert('Hello World')"
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        choices: ["function = myFunction()", "function myFunction()", "function:myFunction()"],
+        answer: "function myFunction()"
+    },
+    {
+        question: "How can you add a comment in a JavaScript?",
+        choices: ["<!--This is a comment-->", "'This is a comment", "//This is a comment"],
+        answer: "//This is a comment"
+    },
+    {
+        question: "How do you declare a JavaScript variable?",
+        choices: ["var carName;", "variable carName;", " v carName;"],
+        answer: "var carName;"
+    },
+    
+    // more questions here
+];
